e49ac205f39c446b1e52498e1c708d9aad89e81a
rahulravindran27091 <rahulravindran2709@rediff.com>
2014-12-30 05:25:07 +0000

    Working chat functionality with angularjs

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var Faye = require('faye');
+var bayeux = new Faye.NodeAdapter({ mount: '/faye', timeout: 45 });
+var client = bayeux.getClient();
 var routes = require('./routes/index');
 var users = require('./routes/users');
+//var message = require('./routes/message');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('bayeux',bayeux);
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,6 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+/* POST message*/
+app.post('/message', function(req, res) {
+    app.get('bayeux').getClient().publish('/channel', { text: req.body.message });
+    console.log('broadcast message:' + req.body.message);
+    res.status(200).send({ message: 'Success' });
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,7 +63,8 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
-});
 
+    
+});
 
 module.exports = app;
